026974fde46502b4885429059f55ddf279e7dc7d
Nafisa Tanta <nafisantanta@gmail.com>
2023-05-06 23:52:53 -0400

    ignore

---
+++ src/config/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_ILRvbKB8N_VJpeAPjXI7cmpxAwHNRSk",
+  authDomain: "animemania-3ff92.firebaseapp.com",
+  databaseURL: "https://animemania-3ff92-default-rtdb.firebaseio.com",
+  projectId: "animemania-3ff92",
+  storageBucket: "animemania-3ff92.appspot.com",
+  messagingSenderId: "908194910975",
+  appId: "1:908194910975:web:0375ec669785052fd132ca",
+  measurementId: "G-09WRZ5G0XW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/pages/LogInPage/login.js
+++ src/pages/LogInPage/login.js
@@ -2,8 +2,18 @@ import React from "react";
 import logo from "../../logo.png";
 import "./login.css";
 import ButtonComponent from "../../components/ButtonComponent";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 function Login() {
+
+    // Login with Google
+    const loginWithGoogle = () => {
+      firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((userCredential) => {
+        console.log(userCredential);
+      });
+    }
+
     return (
         <>
       <div className="loginTop">
@@ -26,6 +36,10 @@ function Login() {
         </div>
         <ButtonComponent type="submit" className="loginButton">
           Log in
+        </ButtonComponent>
+
+        <ButtonComponent onClick={loginWithGoogle} type="submit" className="loginButton">
+          Login with Google
         </ButtonComponent>
         <div className="signupLink">
              Don't have an account? <a href="/signup">Sign up</a>
